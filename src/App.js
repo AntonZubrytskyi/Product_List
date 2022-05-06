eaf42d236eb847ff39d9748ccc96db6faf97244e
AntonZubrytskyi <mranton19@gmail.com>
2022-05-06 12:16:15 +0300

    Create store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
-
+import { useDispatch, useSelector} from "react-redux"
 function App() {
+
+
+  const products = useSelector(state => state.products);
+  console.log(products);
   return (
     <div className="App">
      
